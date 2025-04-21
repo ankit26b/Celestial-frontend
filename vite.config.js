db3f01df35b6e95d7d0073922272a82a7bc68d7a
Ankit Barmase <ankitbarmase26@gmail.com>
2025-04-21 21:22:05 +0530

    vc update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@config': path.resolve(__dirname, 'config'),
+    },
+  },
   plugins: [ react() ],
   server: {
     headers: {
